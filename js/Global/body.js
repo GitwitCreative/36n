0387c8793f14ff651990f2a43f8725f35ee691d5
Zach Miles <zmanmiles@gmail.com>
2021-05-26 18:57:43 +0000

    Moved the navbar logic here

--- js/Global/body.js
+++ js/Global/body.js
@@ -1,5 +1,24 @@
 var Webflow = Webflow || [];
-Webflow.push(function () {});
+Webflow.push(function () {
+  const $navbar = $(".navbar-wrapper");
+  const $logo = $("#nav-logo-transparent");
+  var scroll_start = 0;
+  const offset = $navbar.offset();
+
+  Webflow.scroll.on(function () {
+    scroll_start = $(this).scrollTop();
+    //offset.top
+    if (scroll_start > 50) {
+      if (!$navbar.hasClass("active")) {
+        $navbar.addClass("active");
+        $logo.attr("src", "https://uploads-ssl.webflow.com/603e8caef70bd86255288414/609079308bfb310987247e02_logo-dark.png");
+      }
+    } else {
+      $navbar.removeClass("active");
+      $logo.attr("src", "https://uploads-ssl.webflow.com/603e8caef70bd86255288414/60907930b0d5a66f31e930c5_logo-light.png");
+    }
+  });
+});
 
 var cleanName = function (str) {
   if (!str) {
